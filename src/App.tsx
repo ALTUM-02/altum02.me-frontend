131d5cad5356910f249c5c1fa87504a219a32f9c
ALTUM-02 <martin249jesus@gmail.com>
2026-05-03 14:49:17 +0300

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Hero from "./section/Hero";
+import Navbar from "./section/Navbar";
 
 function App() {
   return (
     <div className="bg-slate-900 text-white">
       <h1>working</h1>
+      <Hero />
+      <Navbar />
     </div>
   );
 }

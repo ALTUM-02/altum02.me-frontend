9a52d8aa7934c50be696072208267fd056a1fd65
ALTUM-02 <martin249jesus@gmail.com>
2026-05-06 09:18:34 +0300

    new change

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import Background from "./components/Background";
-import About from "./section/About";
-import Contact from "./section/Contact";
-import Hero from "./section/Hero";
-import Navbar from "./section/Navbar";
-import Projects from "./section/Projects";
+import About from "./sections/About";
+import Contact from "./sections/Contact";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import Projects from "./sections/Projects";
 
 function App() {
   return (
